2bc706df61f0eb58be7f356472c2b627d145ca0c
insk-lee <inseok.lee@jbnu.ac.kr>
2026-04-26 21:44:24 +0900

    Good Bye, BOJ!

---
+++ Good_Bye_BOJ.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, BOJ!\n");
+
+    printf("Thank You, BOJ!\n");
+    printf("Good Bye, BOJ!\n");
+
+    return 0;
+}
